4af1090bc794d29ec4e0f20cb611f6a6805ad0b4
Ambs04 <amberleejones2007@gmail.com>
2026-01-19 17:37:02 +0200

    add hero section image

--- paper-trails-demo/src/App.jsx
+++ paper-trails-demo/src/App.jsx
@@ -1,9 +1,11 @@
 import Header from "./Modules/CommonComponents/Header";
+import Hero from "./Modules/ModuleComponents/Hero";
 
 function App() {
   return (
     <>
       <Header />
+      <Hero />
     </>
   );
 }
---
+++ paper-trails-demo/src/Modules/ModuleComponents/Hero.jsx
@@ -0,0 +1,11 @@
+import heroImg from "../../assets/hero_image.jpg";
+
+export default function Hero() {
+  return (
+    <>
+      <div>
+        <img src={heroImg} />
+      </div>
+    </>
+  );
+}
