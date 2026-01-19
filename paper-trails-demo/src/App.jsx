5bf5bebfa4a59d6c1cc535a134cd65c1b691f79e
Ambs04 <amberleejones2007@gmail.com>
2026-01-19 17:16:16 +0200

    complete basic layout of header component and add hero section image

--- paper-trails-demo/src/App.jsx
+++ paper-trails-demo/src/App.jsx
@@ -3,7 +3,6 @@ import Header from "./Modules/CommonComponents/Header";
 function App() {
   return (
     <>
-      <h1>hello world</h1>
       <Header />
     </>
   );
--- paper-trails-demo/src/Modules/CommonComponents/Header.jsx
+++ paper-trails-demo/src/Modules/CommonComponents/Header.jsx
@@ -1,15 +1,41 @@
 import logo from "../../assets/logo.png";
-import profile from "../..assets/profile.jpg";
+import profileImg from "../../assets/user.png";
 
 export default function Header() {
   return (
     <>
-      <div>
-        <div>
-          <img src={logo} />
+      <div
+        style={{
+          borderBottom: "2px solid black",
+          width: "100%",
+          height: "30%",
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "space-between",
+        }}
+      >
+        <div
+          style={{
+            display: "flex",
+            width: "250px",
+            marginLeft: "0px",
+            marginBottom: "0px",
+            marginTop: "0px,",
+          }}
+        >
+          <img src={logo} style={{ width: "100%" }} />
         </div>
-        <div>
-          <img src={profile} />
+        <div
+          style={{
+            display: "flex",
+            width: "100px",
+            height: "100px",
+            alignSelf: "center",
+          }}
+        >
+          <button style={{ background: "white" }}>
+            <img src={profileImg} style={{ width: "100%" }} />
+          </button>
         </div>
       </div>
     </>
