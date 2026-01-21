bbe57abbbc624c8f9004877bc39487b6423857b0
Ambs04 <amberleejones2007@gmail.com>
2026-01-21 14:56:35 +0200

    deploy to firebase

--- paper-trails-demo/src/App.jsx
+++ paper-trails-demo/src/App.jsx
@@ -11,6 +11,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
+        {/*<Route path="/dashboard" element={<Dashboard/>>}/>*/}
       </Routes>
     </>
   );
