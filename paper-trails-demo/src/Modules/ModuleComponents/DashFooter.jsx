c3bfea2a6da8e4a07d74f8d235d543d07d04269c
Ambs04 <amberleejones2007@gmail.com>
2026-01-22 12:11:46 +0200

    creating dashboard footer with placeholders

---
+++ paper-trails-demo/src/Modules/ModuleComponents/DashFooter.jsx
@@ -0,0 +1,18 @@
+export default function DashFooter() {
+  <>
+    <div>
+      <div>
+        <p>DASH</p>
+      </div>
+      <div>
+        <p>CUSTOMERS</p>
+      </div>
+      <div>
+        <p>SERVICES</p>
+      </div>
+      <div>
+        <p>USERS</p>
+      </div>
+    </div>
+  </>;
+}
--- paper-trails-demo/src/Pages/Dashboard.jsx
+++ paper-trails-demo/src/Pages/Dashboard.jsx
@@ -1,11 +1,95 @@
 import Greeting from "../Modules/CommonComponents/Greeting";
+import Header from "../Modules/CommonComponents/Header";
+import DashFooter from "../Modules/ModuleComponents/DashFooter";
 
 export default function Dashboard() {
   return (
     <>
-      <h1>
+      <div>
+        <Header />
+      </div>
+      <h3>
         <Greeting />
-      </h1>
+      </h3>
+      <h4>ACCOUNT TYPE</h4>
+      <p></p>
+      <div>
+        <h4>INVOICES</h4>
+        <div>
+          <div>
+            <p></p>
+            <br />
+            <p>INVOICES SENT</p>
+          </div>
+          <div>
+            <p></p>
+            <br />
+            <p>PAID</p>
+          </div>
+          <div>
+            <p></p>
+            <br />
+            <p>UNPAID</p>
+          </div>
+        </div>
+        <h4>CUSTOMERS</h4>
+        <div>
+          <div>
+            <p></p>
+            <br />
+            <p>COUNT</p>
+          </div>
+          <div>
+            <p></p>
+            <br />
+            <p>ACTIVE</p>
+          </div>
+          <div>
+            <p></p>
+            <br />
+            <p>INACTIVE</p>
+          </div>
+        </div>
+        <h4>SERVICES & PRODUCTS</h4>
+        <div>
+          <div>
+            <p></p>
+            <br />
+            <p>COUNT</p>
+          </div>
+          <div>
+            <p></p>
+            <br />
+            <p>AVAILABLE</p>
+          </div>
+          <div>
+            <p></p>
+            <br />
+            <p>UNAVAILABLE</p>
+          </div>
+        </div>
+        <h4>USERS</h4>
+        <div>
+          <div>
+            <p></p>
+            <br />
+            <p>COUNT</p>
+          </div>
+          <div>
+            <p></p>
+            <br />
+            <p>ACTIVE</p>
+          </div>
+          <div>
+            <p></p>
+            <br />
+            <p>INACTIVE</p>
+          </div>
+        </div>
+      </div>
+      <div>
+        <DashFooter />
+      </div>
     </>
   );
 }
