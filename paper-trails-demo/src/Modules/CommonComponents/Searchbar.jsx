f56a51e2e44458f2b2171a547ae37ef1eda039c4
Ambs04 <amberleejones2007@gmail.com>
2026-01-23 15:57:40 +0200

    create searchbar component

---
+++ paper-trails-demo/src/Modules/CommonComponents/Searchbar.jsx
@@ -0,0 +1,11 @@
+export default function Searchbar() {
+  return (
+    <>
+      <div>
+        <div>
+          <input type="text" style={{ border: "1px solid black" }} />
+        </div>
+      </div>
+    </>
+  );
+}
