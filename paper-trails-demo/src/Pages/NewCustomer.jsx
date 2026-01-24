b6cf93fe8448c0bd291358f877bd688fbf8bc429
Ambs04 <amberleejones2007@gmail.com>
2026-01-24 12:12:51 +0200

    set out basic layout for new customer page

--- paper-trails-demo/src/Pages/NewCustomer.jsx
+++ paper-trails-demo/src/Pages/NewCustomer.jsx
@@ -4,11 +4,88 @@ export default function NewCustomer() {
   return (
     <>
       <div>
-        <Link to="/customers">
-          <button>X</button>
-        </Link>
+        <div>
+          <Link to="/customers">
+            <button>X</button>
+          </Link>
+        </div>
+        <h1>add new customer</h1>
       </div>
-      <h1>add new customer</h1>
+      <div>
+        <h3>BASIC ACCOUNT INFORMATION</h3>
+      </div>
+      {/*form section*/}
+      <div>
+        {/*company/person name */}
+        <div>
+          <p></p>
+          <input />
+        </div>
+        {/*contact name */}
+        <div>
+          <p></p>
+          <input />
+        </div>
+        {/*email */}
+        <div>
+          <p></p>
+          <input />
+        </div>
+        {/* phone number*/}
+        <div>
+          <p></p>
+          <input />
+        </div>
+        {/*address */}
+        <div>
+          <p></p>
+          <input />
+        </div>
+        {/*code */}
+        <div>
+          <p></p>
+          <input />
+        </div>
+        {/*payment terms */}
+        <div>
+          {/* 30 days*/}
+          <div>
+            <p></p>
+            <input />
+          </div>
+          {/*14 days */}
+
+          <div>
+            <p></p>
+            <input />
+          </div>
+          {/* 7 days*/}
+
+          <div>
+            <p></p>
+            <input />
+          </div>
+          {/* Cash on delivery*/}
+
+          <div>
+            <p></p>
+            <input />
+          </div>
+        </div>
+        {/*account status */}
+        <div>
+          {/*active */}
+          <div>
+            <p></p>
+            <input />
+          </div>
+          {/*inactive */}
+          <div>
+            <p></p>
+            <input />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
