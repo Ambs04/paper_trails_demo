f6ee0b219d4e86d0b97466788e36f4ecb0ddb081
Ambs04 <amberleejones2007@gmail.com>
2026-01-25 23:59:34 +0200

    add fetch with get and post methods (needs debugging)

--- paper-trails-demo/src/Pages/AddNewProd.jsx
+++ paper-trails-demo/src/Pages/AddNewProd.jsx
@@ -1,14 +1,104 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { baseUrl } from "../api";
 
 export default function AddNewProd() {
+  const [prodStatus, setProdStatus] = useState(true);
+  const nav = useNavigate();
+  const [prodData, setProdData] = useState({
+    prodName: "",
+    prodDesc: "",
+    prodPrice: "",
+    status: prodStatus === true ? "Active" : "Inactive",
+  });
+
+  const handleAddProd = (e) => {
+    const { name, value } = e.target;
+    setProdData((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleProdSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const res = await fetch(`${baseUrl}/productServices`, {
+        method: "POST",
+      });
+      if (res.ok) {
+        alert("Successfully added product!");
+        nav("/products");
+      } else {
+        alert("Failed to add product.");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+      alert("Server is offline.");
+    }
+  };
+
   return (
     <>
       <div>
-        <Link to="/products">
-          <button>X</button>
-        </Link>
+        <div>
+          <Link to="/products">
+            <button>X</button>
+          </Link>
+        </div>
+        <h1>add new product/service</h1>
       </div>
-      <h1>add new product/service</h1>
+      <form onSubmit={handleProdSubmit}>
+        <div>
+          <p>Service/Product Name</p>
+          <input
+            name="prodName"
+            placeholder="e.g clean"
+            type="text"
+            onChange={handleAddProd}
+            required
+          />
+        </div>
+        <div>
+          <p>Description</p>
+          <input
+            name="prodDesc"
+            placeholder="e.g cleaning workplace"
+            type="text"
+            onChange={handleAddProd}
+          />
+        </div>
+        <div>
+          <p>Price: R</p>
+          <input
+            name="prodPrice"
+            placeholder="150"
+            type="text"
+            onChange={handleAddProd}
+            required
+          />
+        </div>
+        <div>
+          <h2>STATUS</h2>
+          <select
+            name="status"
+            value={prodData.status}
+            onChange={handleAddProd}
+          >
+            <option value="active" onChange={() => setProdStatus(true)}>
+              Active
+            </option>
+
+            <option value="inactive" onChange={() => setProdStatus(false)}>
+              Inactive
+            </option>
+          </select>
+          <div>
+            <button type="submit">SUBMIT</button>
+          </div>
+        </div>
+      </form>
     </>
   );
 }
--- paper-trails-demo/src/Pages/Products.jsx
+++ paper-trails-demo/src/Pages/Products.jsx
@@ -3,11 +3,27 @@ import DashFooter from "../Modules/ModuleComponents/DashFooter";
 import Searchbar from "../Modules/CommonComponents/Searchbar";
 import { Link } from "react-router-dom";
 import { baseUrl } from "../api";
+import { useState, useEffect } from "react";
 
 export default function Products() {
-  fetch(`${baseUrl}/productServices`)
-    .then((res) => res.json())
-    .then((data) => console.log(data));
+  const [prods, setProds] = useState([]);
+
+  useEffect(() => {
+    const fetchProds = async () => {
+      try {
+        const res = await fetch(`${baseUrl}/productServices`, {
+          method: "GET",
+        });
+        if (res.ok) {
+          const data = await res.json();
+          setProds(data);
+        }
+      } catch {
+        alert("Failed to fetch products.");
+      }
+    };
+    fetchProds();
+  }, []);
 
   return (
     <>
@@ -21,6 +37,30 @@ export default function Products() {
             <button>+</button>
           </Link>
         </div>
+      </div>
+      <div>
+        {prods.length === 0 ? (
+          <p>No products found.</p>
+        ) : (
+          prods.map((product) => (
+            <div key={product._id}>
+              <div>
+                <h3>Product / Service</h3>
+                <p>{product.prodName}</p>
+
+                <h3>Description</h3>
+                <p>{product.prodDesc}</p>
+
+                <h3>Price: </h3>
+                <p>R{product.prodPrice}</p>
+              </div>
+              <div>
+                <h3>Status</h3>
+                <p>{product.status}</p>
+              </div>
+            </div>
+          ))
+        )}
       </div>
       <div>
         <DashFooter />
