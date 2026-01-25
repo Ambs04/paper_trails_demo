97a9fd483ff992df1366f9cc24c66bed054bf471
Ambs04 <amberleejones2007@gmail.com>
2026-01-26 01:15:47 +0200

    create layout for profile page

--- paper-trails-demo/src/Pages/Profile.jsx
+++ paper-trails-demo/src/Pages/Profile.jsx
@@ -7,9 +7,111 @@ export default function Profile() {
       <div>
         <Header />
       </div>
-      <h3>
-        <Greeting />
-      </h3>
+      <div>
+        <h3>
+          <Greeting />
+        </h3>
+      </div>
+      <div>
+        <p>Subscription plan</p>
+        <div>
+          <button>FREE PLAN</button>
+          <button>SMALL PLAN</button>
+          <button>BIG PLAN</button>
+        </div>
+      </div>
+      <div>
+        <p>INVOICE TEMPLATE</p>
+        <div>
+          <button>FREE OPTION 1</button>
+          <button>PAID VERSIONS</button>
+        </div>
+      </div>
+      <div>
+        <p>
+          The information below should be updated at all times as they will be
+          used to generate invoices.
+        </p>
+        <div>
+          <p>BUSINESS PROFILE INFORMATION</p>
+        </div>
+        <div>
+          <div>
+            <p>Company name:</p>
+            <input />
+          </div>
+          <div>
+            <p>company email:</p>
+            <input type="email" />
+          </div>
+          <div>
+            <p>company contact number:</p>
+            <input />
+          </div>
+          <div>
+            <p>company address:</p>
+            <input />
+          </div>
+          <div>
+            <p>company logo:</p>
+            <input />
+          </div>
+          <div>
+            <p>company colors</p>
+            <input type="color" />
+            <input type="color" />
+          </div>
+        </div>
+        <div>
+          <p>BANKING DETAILS</p>
+        </div>
+        <div>
+          <p>bank</p>
+          <input />
+        </div>
+        <div>
+          <p>Account type:</p>
+          <label>
+            Savings <input type="radio" />
+          </label>
+          <label>
+            Cheque <input type="radio" />
+          </label>
+        </div>
+        <div>
+          <p>account no:</p>
+          <input />
+        </div>
+        <div>
+          <p>account name</p>
+          <input />
+        </div>
+      </div>
+      <div>
+        <p>COMPANY OWNER PROFILE</p>
+      </div>
+      <div>
+        <div>
+          <p>First name</p>
+          <input />
+        </div>
+        <div>
+          <p>last name</p>
+          <input />
+        </div>
+        <div>
+          <p>email</p>
+          <input />
+        </div>
+        <div>
+          <p>contact number</p>
+          <input />
+        </div>
+      </div>
+      <div>
+        <button>UPDATE</button>
+        <button>REQUEST SUPPORT</button>
+      </div>
     </>
   );
 }
