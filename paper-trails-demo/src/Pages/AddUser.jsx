fc7a3bdee54866f53b0504ca07393c44290ca1c7
Ambs04 <amberleejones2007@gmail.com>
2026-01-26 00:59:13 +0200

    complete layout for AddUser page

--- paper-trails-demo/src/Pages/AddUser.jsx
+++ paper-trails-demo/src/Pages/AddUser.jsx
@@ -4,11 +4,55 @@ export default function AddUser() {
   return (
     <>
       <div>
-        <Link to="/users">
-          <button>X</button>
-        </Link>
+        <div>
+          <Link to="/users">
+            <button>X</button>
+          </Link>
+        </div>
+        <h1>add new user</h1>
       </div>
-      <h1>add new user</h1>
+      <div>
+        <h3>BASIC ACCOUNT INFORMATION</h3>
+      </div>
+      {/*form section*/}
+      <div>
+        {/*first name */}
+        <div>
+          <p>First Name:</p>
+          <input name="userFirstName" />
+        </div>
+        {/*last name */}
+        <div>
+          <p>Last Name:</p>
+          <input name="userLastName" />
+        </div>
+        {/*email */}
+        <div>
+          <p>Email:</p>
+          <input name="userEmail" type="email" placeholder="james@email.com" />
+        </div>
+        {/* phone number*/}
+        <div>
+          <p>Phone:</p>
+          <input name="userPhone" placeholder="078 888 8888" />
+        </div>
+        <div>
+          <p>Password:</p>
+          <input type="text" placeholder="124578" />
+        </div>
+        <div>
+          <p>Confirm Password:</p>
+          <input type="text" placeholder="124578" />
+        </div>
+        <div>
+          <div>
+            <button>SUBMIT</button>
+          </div>
+          <div>
+            <button>CANCEL</button>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
