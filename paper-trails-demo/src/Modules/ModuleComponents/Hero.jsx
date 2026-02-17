a223343c9f42b208bcd773d41d7d84bbab9dfe03
Ambs04 <amberleejones2007@gmail.com>
2026-02-17 12:28:36 +0200

    add styling for hero and home section

--- paper-trails-demo/src/Modules/ModuleComponents/Hero.jsx
+++ paper-trails-demo/src/Modules/ModuleComponents/Hero.jsx
@@ -1,10 +1,29 @@
 import heroImg from "../../assets/hero_image.jpg";
+import "../../Styles/hero.css";
 
 export default function Hero() {
   return (
     <>
-      <div>
-        <img src={heroImg} />
+      <div
+        id="hero-container"
+        style={{ position: "relative", overflow: "hidden" }}
+      >
+        <img src={heroImg} style={{ opacity: "0.4" }} />
+        <h1
+          style={{
+            position: "absolute",
+            top: "35%",
+            bottom: "50%",
+            textAlign: "center",
+
+            width: "100%",
+            zIndex: "2",
+            color: "#652912",
+            textShadow: "5px -5px 8px rgba(175, 61, 29, 0.45) ",
+          }}
+        >
+          INVOICING MADE SIMPLE
+        </h1>
       </div>
     </>
   );
--- paper-trails-demo/src/Pages/Home.jsx
+++ paper-trails-demo/src/Pages/Home.jsx
@@ -1,20 +1,36 @@
 import Header from "../Modules/CommonComponents/Header";
 import Hero from "../Modules/ModuleComponents/Hero";
 import { Link } from "react-router-dom";
+import "../Styles/home.css";
 
 export default function Home() {
   return (
     <>
-      <Header />
-      <Hero />
-      <p>
-        A simpler way to handle all your admin. Send invoices, track payments
-        and manage clients. All this in the palm of your hand.
-      </p>
-      <div>
-        <Link to="/signup">
-          <button>Sign Up now!</button>
-        </Link>
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "1fr",
+          gridTemplateRows: "repeat(4, 1fr)",
+          maxHeight: "950px",
+        }}
+      >
+        <div>
+          <Header style={{ display: "grid", gridArea: "1/1/2/2" }} />
+        </div>
+        <div style={{ display: "grid", gridArea: "2/1/3/2" }}>
+          <Hero />
+        </div>
+        <div id="home-text" style={{ display: "grid", gridArea: "3/1/4/2" }}>
+          <p>
+            A simpler way to handle all your admin. Send invoices, track
+            payments and manage clients. All this in the palm of your hand.
+          </p>
+        </div>
+        <div style={{ display: "grid", gridArea: "4/1/5/2" }}>
+          <Link to="/signup">
+            <button id="sign-up-btn">Sign Up now!</button>
+          </Link>
+        </div>
       </div>
     </>
   );
